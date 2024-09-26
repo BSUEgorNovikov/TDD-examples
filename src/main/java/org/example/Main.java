2ce48ccd279a52a71411ad96b75d5df8ed08306c
BSUEgorNovikov <egornovikov8060@gmail.com>
2024-09-26 13:17:32 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Multiply(12, 3));
+    }
+
+    public static int Multiply(int a, int b) {
+        return a*b;
+    }
+}
