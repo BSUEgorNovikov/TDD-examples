82f68dfa1df2d0ab6cf3a8cd958499cf79aa2039
BSUEgorNovikov <egornovikov8060@gmail.com>
2024-09-26 13:46:42 +0300

    part1 (1,2)

---
+++ src/main/java/org/example/Dollar.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Dollar {
+
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+}
