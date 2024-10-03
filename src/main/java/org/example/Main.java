8eb0f9615c96b60a541723d18de92846261a4392
BSUEgorNovikov <egornovikov8060@gmail.com>
2024-10-03 13:43:43 +0300

    part 1 (3)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Multiply(12, 3));
-    }
 
-    public static int Multiply(int a, int b) {
-        return a*b;
     }
 }
