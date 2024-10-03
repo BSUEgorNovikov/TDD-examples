469f694881e6995c65eee84ca39fa17933910fff
BSUEgorNovikov <egornovikov8060@gmail.com>
2024-10-03 14:04:31 +0300

    part 1 (5)

---
+++ src/test/java/org/example/FrancTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FrancTest {
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+}
