c476aea7be5036982b0f1eb4dbd6821c8bd5dd07
BSUEgorNovikov <egornovikov8060@gmail.com>
2024-10-04 16:41:34 +0300

    part 1 (11)

--- src/test/java/org/example/MoneyTest.java
+++ src/test/java/org/example/MoneyTest.java
@@ -5,12 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
     @Test
     public void testEquality() {
         assertTrue(Money.dollar(5). equals(Money.dollar(5)));
         assertFalse(Money.dollar(5). equals(Money.dollar(6)));
-        assertTrue(Money.franc(5). equals(Money.franc(5)));
-        assertFalse(Money.franc(5). equals(Money.franc(6)));
         assertFalse(Money.franc(5). equals(Money.dollar(5)));
     }
 
@@ -19,11 +25,4 @@ public class MoneyTest {
         assertEquals("USD", Money.dollar(1). currency());
         assertEquals("CHF", Money.franc(1). currency());
     }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF"). equals(new Franc(10, "CHF")));
-        assertTrue(new Money(10, "USD"). equals(new Dollar(10, "USD")));
-    }
-
 }
