b453e6edfddc3792a34c29abedfd2ff01d33275b
BSUEgorNovikov <egornovikov8060@gmail.com>
2024-10-04 16:17:50 +0300

    part 1 (9)

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -2,13 +2,18 @@ package org.example;
 
 abstract class Money {
     protected int amount;
+    protected String currency;
 
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
@@ -17,4 +22,7 @@ abstract class Money {
     }
 
     abstract Money times(int multiplier);
+    String currency() {
+        return currency;
+    }
 }
